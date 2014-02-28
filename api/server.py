71be9c09b2293248aaed42855739836b57d13e1f
kgriffs <kurt.griffiths@rackspace.com>
2014-02-28 14:06:03 -0600

    Added basic video upload form

--- api/server.py
+++ api/server.py
@@ -1,16 +1,33 @@
+import cgi
+from wsgiref import simple_server
+
 import falcon
 
 
 class VideoCollection(object):
 
-    def on_get(self):
+    def on_get(self, req, resp):
         # TODO(kgriffs): List videos
+        pass
 
-    def on_post(self):
+    def on_post(self, req, resp):
+        form = cgi.FieldStorage(fp=req.stream, environ=req.env)
+        title, video = form['title'].value, form['video'].file
+
+        # if video_field.file:
+        #     pass
+
+        data = video.read()
+
         # TODO(kgriffs): Stream into Swift
         # TODO(kgriffs): On successful stream, create video document in mongo
 
 
-app = falcon.API()
+app = application = falcon.API()
 
 app.add_route('/videos', VideoCollection())
+
+if __name__ == '__main__' :
+    httpd = simple_server.make_server('localhost', 8000, app)
+    print 'Serving on localhost:8000'
+    httpd.serve_forever()
