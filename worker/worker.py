fc20c37bb891acb16c2b1689245eb514f4bfbaa8
Curtis Carter <curtis@rubyhq.com>
2014-02-28 16:30:55 -0600

    started on worker

--- worker/worker.py
+++ worker/worker.py
@@ -1,20 +1,23 @@
 import getpass
 import tempfile
 import time
+import uuid
 
 import pyrax
 
-
+#WORK IN PROGRESS
 class Worker(object):
 
-    def __init__(self, user, key):
-        pass
+    def __init__(self, user, key, unprocessed_container, processed_container, queue):
+        self.unprocessed_container = unprocessed_container
+        self.processed_container = processed_container
+        self.queue = queue
 
     def _transcode(input_file, output_file):
         pass
 
     def _get_work():
-        pass
+        claim = self.queue.claim_messages(1800, 1800, 1)
 
     def run():
         while True:
@@ -28,11 +31,19 @@ class Worker(object):
                 time.sleep(1)
 
 
+#TODO Duped from server
+def _generate_id():
+    return str(uuid.uuid1())
+
 if __name__ == '__main__':
     user = 'rackervision'
     key = getpass.getpass('API Key: ')
+    pyrax.queues.client_id = _generate_id()
+    un_c = pyrax.cloudfiles.get_container("unprocessed")
+    proc_c = pyrax.cloudfiles.get_container("processed")
+    queue = pyrax.queues.get("unprocessed")
 
     pyrax.set_setting('identity_type', 'rackspace')
     pyrax.set_credentials(user, key)
 
-    Worker(user, key).run()
+    Worker(user, key, un_c, proc_c, queue).run()
