381ce6840c596d1bcd5643ca2a0158c9c919c306
kgriffs <kurt.griffiths@rackspace.com>
2014-02-28 16:20:19 -0600

    Implemented list videos

--- api/server.py
+++ api/server.py
@@ -1,22 +1,41 @@
 import cgi
+import time
+import json
+import uuid
 from wsgiref import simple_server
 
 import falcon
-import uuid
+import pymongo
 import pyrax
+
+
+def _generate_id():
+    return str(uuid.uuid1())
+
+
+def _now():
+    return time.time()
+
 
 class VideoCollection(object):
 
-    def __init__(self):
-        pyrax.keyring_auth("rackervision")
+    def __init__(self, db):
+        self.db = db
+
+        pyrax.set_setting('identity_type', 'rackspace')
+        pyrax.keyring_auth('rackervision')
         pyrax.queues.client_id = _generate_id()
+
         self.unprocessed_container = pyrax.cloudfiles.get_container("unprocessed")
         self.processed_container = pyrax.cloudfiles.get_container("processed")
         self.queue = pyrax.queues.get("unprocessed")
 
     def on_get(self, req, resp):
-        # TODO(kgriffs): List videos
-        pass
+        sort = [('uploaded_at', pymongo.DESCENDING)]
+        cursor = self.db.videos.find({}, fields={'_id': False}, sort=sort)
+
+        doc = list(cursor)
+        resp.body = json.dumps(doc)
 
     def on_post(self, req, resp):
         form = cgi.FieldStorage(fp=req.stream, environ=req.env)
@@ -25,22 +44,29 @@ class VideoCollection(object):
         # if video_field.file:
         #     pass
 
+        # Stream into Swift
         uuid = _generate_id()
         self.unprocessed_container.upload_file(video, uuid)
         self.queue.post_message(uuid, 1209599)
-        
 
-        # TODO(kgriffs): Stream into Swift
-        # TODO(kgriffs): On successful stream, create video document in mongo
+        # On successful stream, create video document in mongo
+        doc = {
+            'title': title,
+            'uuid': uuid,
+            'href': None,
+            'uploaded_at': _now(),
+        }
 
+        self.db.videos.insert(doc)
 
-def _generate_id():
-    return str(uuid.uuid1())
+# ---------------------------------------------------------------------------
 
 
 app = application = falcon.API()
 
-app.add_route('/videos', VideoCollection())
+mongo_client = pymongo.MongoClient()
+app.add_route('/videos', VideoCollection(mongo_client['rackervision']))
+
 
 if __name__ == '__main__' :
     httpd = simple_server.make_server('localhost', 8000, app)
